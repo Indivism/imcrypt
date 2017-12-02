45f02dad4b91a07f3f38b11425d5862a605f3266
Cheng <chenghang8888@gmail.com>
2017-12-02 15:36:35 -0600

    Beginning UI components.

---
+++ src/imcrypt/encoderGUI.java
@@ -0,0 +1,12 @@
+package imcrypt;
+import java.awt.*;
+import javax.swing.*;
+/**
+ * imcrypt encoder GUI.
+ */
+public class encoderGUI extends JFrame {
+	private static final long serialVersionUID = 1L;
+	JFrame encoderUI = new JFrame ("imcrypt Encoder");
+	
+	//encoderUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
