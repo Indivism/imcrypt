aa257e2097d86d3a1f4e9e39dfcf632ed07d0b8a
Cheng <chenghang8888@gmail.com>
2017-12-02 18:48:51 -0600

    Updated sizing method on encrypt GUI

--- src/imcrypt/encoderGUI.java
+++ src/imcrypt/encoderGUI.java
@@ -9,18 +9,17 @@ import java.io.*;
  * imcrypt encoder GUI.
  */
 public class encoderGUI implements ActionListener{
-	String path;
 	JFrame encoderUI;
     JFileChooser imageChooser;
     JButton browseImage, showImage;
     JTextField imagePath;
     JPanel buttonPanel;
     BufferedImage image;
-    JLabel label;
+    JLabel imageDisplay;
 	
 	public encoderGUI() {
 		JFrame encoderUI = new JFrame ("imcrypt Encoder");
-		label = new JLabel();
+		imageDisplay = new JLabel();
 		buttonPanel = new JPanel();
 		encoderUI.setBounds(500, 250, 750, 500);
 		encoderUI.setPreferredSize(new Dimension(750, 500));
@@ -28,14 +27,14 @@ public class encoderGUI implements ActionListener{
 		encoderUI.setLayout(new FlowLayout());
 		browseImage = new JButton("Browse");
 		browseImage.addActionListener(this);
-		showImage = new JButton("Show Image");
+		showImage = new JButton("Encrypt");
 		showImage.addActionListener(this);
 		imagePath = new JTextField(20);
 		buttonPanel.add(browseImage);
 		buttonPanel.add(showImage);
 		buttonPanel.add(imagePath);
 		encoderUI.add(buttonPanel);
-		encoderUI.add(label);
+		encoderUI.add(imageDisplay);
 		encoderUI.pack();
 		encoderUI.setVisible(true);
 	}
@@ -45,16 +44,8 @@ public class encoderGUI implements ActionListener{
 			imageChooser = new JFileChooser();
 			imageChooser.showOpenDialog(encoderUI);
 			String path = imageChooser.getSelectedFile().getAbsolutePath();
-			imagePath.setText(path);
-			try {
-				image = ImageIO.read(new File(path));
-                ImageIcon icon = new ImageIcon(image);
-                label.setIcon(icon);
-                Dimension imageSize = new Dimension(icon.getIconWidth(),icon.getIconHeight());
-                label.setPreferredSize(imageSize);
-                label.revalidate();
-                label.repaint();
-			} catch(IOException error) {}
+			//imagePath.setText(path);
+            imageDisplay.setIcon(new ImageIcon(new ImageIcon(path).getImage()));
 		}
 	}
 	
